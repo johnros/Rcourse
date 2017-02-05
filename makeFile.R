864d0b3d4975d5308e7878fd4f155a7b3ea16af9
Jonathan Rosenblatt <johnros@bgu.ac.il>
2017-02-05 13:04:02 +0200

    fixed pdf

---
+++ makeFile.R
@@ -0,0 +1,10 @@
+library(bookdown)
+
+
+render_book("index.Rmd","bookdown::gitbook") # render to HTML
+render_book("index.Rmd","bookdown::pdf_book") # rendoer to PDF
+                      
+
+
+
+# preview_chapter()
