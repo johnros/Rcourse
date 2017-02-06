ada3b9f57c28501ad980fd86aa2db178acdddeae
Jonathan Rosenblatt <johnros@bgu.ac.il>
2017-02-06 22:26:27 +0200

    glm

--- makeFile.R
+++ makeFile.R
@@ -4,15 +4,20 @@ render_book("index.Rmd","bookdown::gitbook") # render to HTML
 render_book("index.Rmd","bookdown::word_document2") # rendoer to DOCX
 render_book("index.Rmd","bookdown::pdf_book") # rendoer to PDF
 
+preview_chapter('02-r-basics.Rmd',"bookdown::gitbook" )
+preview_chapter('02-r-basics.Rmd',"bookdown::pdf_book" )
+
+preview_chapter('02-r-basics.Rmd',"bookdown::gitbook" )
+preview_chapter('02-r-basics.Rmd',"bookdown::pdf_book" )
 
 preview_chapter('03-eda.Rmd',"bookdown::gitbook" )
 preview_chapter('03-eda.Rmd',"bookdown::pdf_book" )
 
 library(magrittr)
 preview_chapter('04-lm.Rmd',"bookdown::gitbook" )
-preview_chapter('04-lm.Rmd',"bookdown::pdf_book" )
 preview_chapter('04-lm.Rmd',"bookdown::word_document2" )
+preview_chapter('04-lm.Rmd',"bookdown::pdf_book" )
 
 preview_chapter('05-glm.Rmd',"bookdown::gitbook" )
-preview_chapter('05-glm.Rmd',"bookdown::pdf_book" )
 preview_chapter('05-glm.Rmd',"bookdown::word_document2" )
+preview_chapter('05-glm.Rmd',"bookdown::pdf_book" )
