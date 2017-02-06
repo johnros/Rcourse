a2f9e2688c78555e382ff7e3b2d1c6a3e772f3ae
Jonathan Rosenblatt <johnros@bgu.ac.il>
2017-02-06 08:17:34 +0200

    lm

--- makeFile.R
+++ makeFile.R
@@ -1,10 +1,7 @@
 library(bookdown)
 
-
 render_book("index.Rmd","bookdown::gitbook") # render to HTML
 render_book("index.Rmd","bookdown::pdf_book") # rendoer to PDF
-                      
-
 
 
 preview_chapter('03-eda.Rmd',"bookdown::gitbook" )
