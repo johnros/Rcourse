7ac6e13feba182f6272aa12ac761d5223809f965
Jonathan Rosenblatt <johnros@bgu.ac.il>
2017-02-08 23:44:23 +0200

    fixing references

--- makeFile.R
+++ makeFile.R
@@ -1,8 +1,15 @@
 library(bookdown)
+library(magrittr)
 
-bookdown::render_book("index.Rmd","bookdown::gitbook") # render to HTML
-bookdown::render_book("index.Rmd","bookdown::word_document2") # rendoer to DOCX
-bookdown::render_book("index.Rmd","bookdown::pdf_book") # rendoer to PDF
+rmarkdown::render_site(output_format = 'bookdown::pdf_book', encoding = 'UTF-8')
+
+bookdown::render_book("index.Rmd","bookdown::gitbook", clean_envir = TRUE) # render to HTML
+bookdown::render_book("index.Rmd","bookdown::word_document2", clean_envir = TRUE) # rendoer to DOCX
+bookdown::render_book("index.Rmd","bookdown::pdf_book", clean_envir = TRUE) # rendoer to PDF
+
+bookdown::preview_chapter('09-unsupervised.Rmd',"bookdown::gitbook" )
+bookdown::preview_chapter('09-unsupervised.Rmd',"bookdown::word_document2" )
+bookdown::preview_chapter('09-unsupervised.Rmd',"bookdown::pdf_book" )
 
 bookdown::preview_chapter('08-supervised.Rmd',"bookdown::gitbook" )
 bookdown::preview_chapter('08-supervised.Rmd',"bookdown::word_document2" )
@@ -24,8 +31,8 @@ bookdown::preview_chapter('04-lm.Rmd',"bookdown::gitbook" )
 bookdown::preview_chapter('04-lm.Rmd',"bookdown::word_document2" )
 bookdown::preview_chapter('04-lm.Rmd',"bookdown::pdf_book" )
 
-bookdown::preview_chapter('03-eda.Rmd',"bookdown::gitbook" )
-bookdown::preview_chapter('03-eda.Rmd',"bookdown::pdf_book" )
+bookdown::preview_chapter('03-eda.Rmd',"bookdown::gitbook" , clean_envir = TRUE)
+bookdown::preview_chapter('03-eda.Rmd',"bookdown::pdf_book", clean_envir = TRUE )
 
 bookdown::preview_chapter('02-r-basics.Rmd',"bookdown::gitbook" )
 bookdown::preview_chapter('02-r-basics.Rmd',"bookdown::pdf_book" )
