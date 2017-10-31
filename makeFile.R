37f08ef79e24c6ce9951c05c2ad2be379ef3e1b3
Jonathan Rosenblatt <johnros@bgu.ac.il>
2017-10-31 06:54:52 +0200

    progress

--- makeFile.R
+++ makeFile.R
@@ -11,6 +11,10 @@ bookdown::render_book("index.Rmd","bookdown::gitbook", clean_envir = TRUE) # ren
 bookdown::render_book("index.Rmd","bookdown::word_document2", clean_envir = TRUE) # rendoer to DOCX
 bookdown::render_book("index.Rmd","bookdown::pdf_book", clean_envir = TRUE) # rendoer to PDF
 
+bookdown::preview_chapter('20-datatable.Rmd',"bookdown::gitbook" )
+bookdown::preview_chapter('20-datatable.Rmd',"bookdown::word_document2" )
+bookdown::preview_chapter('20-datatable.Rmd',"bookdown::pdf_book" )
+
 bookdown::preview_chapter('16-algebra.Rmd',"bookdown::gitbook" )
 bookdown::preview_chapter('16-algebra.Rmd',"bookdown::word_document2" )
 bookdown::preview_chapter('16-algebra.Rmd',"bookdown::pdf_book" )
