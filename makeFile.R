a3231b8be813182fb5bc8ddfa27f9b1fddc18ad3
Jonathan Rosenblatt <johnros@bgu.ac.il>
2018-12-28 17:50:13 +0200

    updated and compiled

--- makeFile.R
+++ makeFile.R
@@ -3,73 +3,21 @@ library(magrittr)
 rm(list=ls())
 
 file.remove('Rcourse.Rmd')
+bookdown::render_book("index.Rmd","bookdown::pdf_book", clean_envir = TRUE, output_dir = 'docs') # render to HTML
 bookdown::render_book("index.Rmd","bookdown::gitbook", clean_envir = TRUE, output_dir = 'docs')
 bookdown::render_book("index.Rmd","bookdown::word_document2", clean_envir = TRUE, output_dir = 'docs')
-bookdown::render_book("index.Rmd","bookdown::pdf_book", clean_envir = TRUE, output_dir = 'docs') # render to HTML
 bookdown::clean_book()
 
 bookdown::render_book("index.Rmd","bookdown::gitbook", clean_envir = TRUE) # render to HTML
 bookdown::render_book("index.Rmd","bookdown::word_document2", clean_envir = TRUE) # rendoer to DOCX
 bookdown::render_book("index.Rmd","bookdown::pdf_book", clean_envir = TRUE) # rendoer to PDF
 
-bookdown::preview_chapter('20-datatable.Rmd',"bookdown::gitbook" )
-bookdown::preview_chapter('20-datatable.Rmd',"bookdown::word_document2" )
-bookdown::preview_chapter('20-datatable.Rmd',"bookdown::pdf_book" )
+files <- list.files(pattern = '.Rmd')
+file.remove('Rcourse.Rmd')
+for(i in 14:length(files)){
+  bookdown::preview_chapter(files[i],"bookdown::gitbook" )
+  readline(prompt="Press [enter] to continue")
+}
+bookdown::preview_chapter(files[2],"bookdown::gitbook" )
 
-bookdown::preview_chapter('16-algebra.Rmd',"bookdown::gitbook" )
-bookdown::preview_chapter('16-algebra.Rmd',"bookdown::word_document2" )
-bookdown::preview_chapter('16-algebra.Rmd',"bookdown::pdf_book" )
-
-bookdown::preview_chapter('15-parallel.Rmd',"bookdown::gitbook" )
-bookdown::preview_chapter('15-parallel.Rmd',"bookdown::word_document2" )
-bookdown::preview_chapter('15-parallel.Rmd',"bookdown::pdf_book" )
-
-bookdown::preview_chapter('14-memory.Rmd',"bookdown::gitbook" )
-bookdown::preview_chapter('14-memory.Rmd',"bookdown::word_document2" )
-bookdown::preview_chapter('14-memory.Rmd',"bookdown::pdf_book" )
-
-bookdown::preview_chapter('13-sparsity.Rmd',"bookdown::gitbook" )
-bookdown::preview_chapter('13-sparsity.Rmd',"bookdown::word_document2" )
-bookdown::preview_chapter('13-sparsity.Rmd',"bookdown::pdf_book" )
-
-bookdown::preview_chapter('12-hadleyverse.Rmd',"bookdown::gitbook" )
-bookdown::preview_chapter('12-hadleyverse.Rmd',"bookdown::word_document2" )
-bookdown::preview_chapter('12-hadleyverse.Rmd',"bookdown::pdf_book" )
-
-bookdown::preview_chapter('11-reporting.Rmd',"bookdown::gitbook" )
-bookdown::preview_chapter('11-reporting.Rmd',"bookdown::word_document2" )
-bookdown::preview_chapter('11-reporting.Rmd',"bookdown::pdf_book" )
-
-bookdown::preview_chapter('10-plotting.Rmd',"bookdown::gitbook" )
-bookdown::preview_chapter('10-plotting.Rmd',"bookdown::word_document2" )
-bookdown::preview_chapter('10-plotting.Rmd',"bookdown::pdf_book" )
-
-bookdown::preview_chapter('09-unsupervised.Rmd',"bookdown::gitbook" )
-bookdown::preview_chapter('09-unsupervised.Rmd',"bookdown::word_document2" )
-bookdown::preview_chapter('09-unsupervised.Rmd',"bookdown::pdf_book" )
-
-bookdown::preview_chapter('08-supervised.Rmd',"bookdown::gitbook" )
-bookdown::preview_chapter('08-supervised.Rmd',"bookdown::word_document2" )
-bookdown::preview_chapter('08-supervised.Rmd',"bookdown::pdf_book" )
-
-bookdown::preview_chapter('07-multivariate.Rmd',"bookdown::gitbook" )
-bookdown::preview_chapter('07-multivariate.Rmd',"bookdown::word_document2" )
-bookdown::preview_chapter('07-multivariate.Rmd',"bookdown::pdf_book" )
-
-bookdown::preview_chapter('06-lme.Rmd',"bookdown::gitbook" )
-bookdown::preview_chapter('06-lme.Rmd',"bookdown::word_document2" )
-bookdown::preview_chapter('06-lme.Rmd',"bookdown::pdf_book" )
-
-bookdown::preview_chapter('05-glm.Rmd',"bookdown::gitbook" )
-bookdown::preview_chapter('05-glm.Rmd',"bookdown::word_document2" )
-bookdown::preview_chapter('05-glm.Rmd',"bookdown::pdf_book" )
-
-bookdown::preview_chapter('04-lm.Rmd',"bookdown::gitbook" )
-bookdown::preview_chapter('04-lm.Rmd',"bookdown::word_document2" )
-bookdown::preview_chapter('04-lm.Rmd',"bookdown::pdf_book" )
-
-bookdown::preview_chapter('30-eda.Rmd',"bookdown::gitbook" , clean_envir = TRUE)
-bookdown::preview_chapter('30-eda.Rmd',"bookdown::pdf_book")
-
-bookdown::preview_chapter('02-r-basics.Rmd',"bookdown::gitbook" )
-bookdown::preview_chapter('02-r-basics.Rmd',"bookdown::pdf_book" )
+bookdown::preview_chapter('98-causality.Rmd',"bookdown::pdf_book" )
