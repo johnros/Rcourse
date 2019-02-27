600f8494922b5deb98eefd5699451e29a06a30c7
Jonathan Rosenblatt <john.ros@gmail.com>
2019-02-27 19:21:38 +0200

    working on R3.5.2

--- makeFile.R
+++ makeFile.R
@@ -1,23 +1,18 @@
-library(bookdown)
-library(magrittr)
 rm(list=ls())
 
 file.remove('Rcourse.Rmd')
+bookdown::render_book("index.Rmd","bookdown::gitbook", output_dir = 'docs')
 bookdown::render_book("index.Rmd","bookdown::pdf_book", clean_envir = TRUE, output_dir = 'docs') # render to HTML
-bookdown::render_book("index.Rmd","bookdown::gitbook", clean_envir = TRUE, output_dir = 'docs')
 bookdown::render_book("index.Rmd","bookdown::word_document2", clean_envir = TRUE, output_dir = 'docs')
 bookdown::clean_book()
 
-bookdown::render_book("index.Rmd","bookdown::gitbook", clean_envir = TRUE) # render to HTML
-bookdown::render_book("index.Rmd","bookdown::word_document2", clean_envir = TRUE) # rendoer to DOCX
-bookdown::render_book("index.Rmd","bookdown::pdf_book", clean_envir = TRUE) # rendoer to PDF
-
 files <- list.files(pattern = '.Rmd')
 file.remove('Rcourse.Rmd')
-for(i in 14:length(files)){
-  bookdown::preview_chapter(files[i],"bookdown::gitbook" )
+for(i in 1:length(files)){
+  bookdown::preview_chapter(files[i],"bookdown::pdf_book" )
   readline(prompt="Press [enter] to continue")
 }
-bookdown::preview_chapter(files[2],"bookdown::gitbook" )
+bookdown::preview_chapter(files[10],"bookdown::pdf_book" )
+bookdown::preview_chapter(files[22],"bookdown::gitbook" )
 
 bookdown::preview_chapter('98-causality.Rmd',"bookdown::pdf_book" )
